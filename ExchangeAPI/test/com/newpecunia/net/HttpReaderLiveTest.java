93bc29be47d1bfcd0217715ed0e28adadffad6f6
Petr <petr.smid@atlas.cz>
2013-08-11 16:22:28 +0100

    Fixing instantiation of HttpReader object.

--- ExchangeAPI/test/com/newpecunia/net/HttpReaderLiveTest.java
+++ ExchangeAPI/test/com/newpecunia/net/HttpReaderLiveTest.java
@@ -1,31 +1,20 @@
 package com.newpecunia.net;
 
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import com.google.inject.Guice;
-import com.google.inject.Injector;
-import com.newpecunia.GuiceBitexchangeModule;
 import com.newpecunia.bitstamp.service.impl.BitstampServiceConstants;
-import com.newpecunia.net.HttpReader;
 
 @RunWith(JUnit4.class)
 public class HttpReaderLiveTest {
 
-	HttpReader httpReader;
-	
-	@Before
-	public void setup() {
-		Injector injector = Guice.createInjector(new GuiceBitexchangeModule());
-		httpReader = injector.getInstance(HttpReader.class);
-	}
+	HttpReader httpReader = new HttpSimpleReaderImpl();
 	
 	@Test
 	public void test() throws IOException {
