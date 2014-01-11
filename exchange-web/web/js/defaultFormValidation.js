b55a7060703f9e364bc16b194db724b0c7007094
Petr <petr.smid@atlas.cz>
2014-01-11 01:20:20 +0100

    Implementation of Buy site

---
+++ exchange-web/web/js/defaultFormValidation.js
@@ -0,0 +1,22 @@
+// override jquery validate plugin defaults to be compatible with Bootstrap
+$.validator.setDefaults({
+    highlight: function (element) {
+        $(element).closest('.form-group').addClass('has-error');
+    },
+    unhighlight: function (element) {
+        $(element).closest('.form-group').removeClass('has-error');
+    },
+    errorElement: 'span',
+    errorClass: 'help-block',
+    errorPlacement: function (error, element) {
+        if (element.parent('.input-group').length) {
+            error.insertAfter(element.parent());
+        } else {
+            error.insertAfter(element);
+        }
+    }
+});
+
+$.validator.addMethod("btcAddress", function(value, element) {
+        return this.optional(element) || /^[a-zA-Z0-9]+$/.test(value);
+	}, "Please enter a valid Bitcoin address."); 
